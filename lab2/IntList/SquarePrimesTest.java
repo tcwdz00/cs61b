9008c43479975a533adfc1e95b9ba6806fd7343f
tcwdz00 <tcwdz00>
2022-06-29 15:47:11 -0400

    done with lab1

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,36 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimes2() {
+        IntList lst = IntList.of(1, 2, 3, 6, 8, 9);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 9 -> 6 -> 8 -> 9", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimes3() {
+        IntList lst = IntList.of(8);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("8", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimes5() {
+        IntList lst = IntList.of(3);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("9", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimes4() {
+        IntList lst = IntList.of(1, 4, 10, 6, 8, 9);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 10 -> 6 -> 8 -> 9", lst.toString());
+        assertFalse(changed);
+    }
 }
